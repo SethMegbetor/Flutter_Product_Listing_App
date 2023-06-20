7b1a23d64a808b8b60abdd0def42265f162aaad1
SethMegbetor <sethmegbetor@gmail.com>
2023-06-20 17:37:35 +0000

    state management
    added rating

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,80 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Product Listing'),
+//       ),
+//       body: Center(
+//         child: GestureDetector(
+//           onTap: () {
+//             _showDialog(context);
+//           },
+//           child: const Text(
+//             'Hello, World!',
+//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//           ),
+//         ),
+//         // child: Text.rich(
+//         //   TextSpan(
+//         //     children: <TextSpan>[
+//         //       TextSpan(
+//         //           text: "Hello ",
+//         //           style: TextStyle(fontStyle: FontStyle.italic)),
+//         //       TextSpan(
+//         //           text: "World", style: TextStyle(fontWeight: FontWeight.bold)),
+//         //     ],
+//         //   ),
+//         // ),
+//       ),
+//     );
+//   }
+// }
+
+// // show dialog function
+// void _showDialog(BuildContext context) {
+//   showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text("Message"),
+//           content: const Text("Hello World"),
+//           actions: <Widget>[
+//             TextButton(
+//               child: const Text("Close"),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//             )
+//           ],
+//         );
+//       });
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,23 +106,6 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Product Listing'),
       ),
-      // body: const Center(
-      //   // child: Text(
-      //   //   'Hello, World!',
-      //   //   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      //   // ),
-      //   child: Text.rich(
-      //     TextSpan(
-      //       children: <TextSpan>[
-      //         TextSpan(
-      //             text: "Hello ",
-      //             style: TextStyle(fontStyle: FontStyle.italic)),
-      //         TextSpan(
-      //             text: "World", style: TextStyle(fontWeight: FontWeight.bold)),
-      //       ],
-      //     ),
-      //   ),
-      // ),
       body: ListView(
         shrinkWrap: true,
         padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
@@ -90,7 +150,7 @@ class ProductBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(2),
-      height: 120,
+      height: 140,
       child: Card(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -106,6 +166,7 @@ class ProductBox extends StatelessWidget {
                         style: const TextStyle(fontWeight: FontWeight.bold)),
                     Text(description),
                     Text("Price: $price"),
+                    RatingBox()
                   ],
                 ),
               ),
@@ -113,6 +174,98 @@ class ProductBox extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class RatingBox extends StatefulWidget {
+  @override
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
     );
   }
 }
